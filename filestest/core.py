8d778125c2b0da9777cfb4b251c5b6624b291a80
leedabee <leedabee>
2021-10-04 21:46:35 +0000

    test for imports

---
+++ filestest/core.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+# -*- coding: utf-8 -*-
+from . import helpers
+
+def get_hmm():
+    """Get a thought."""
+    return 'hmmm...'
+
+
+def hmm():
+    """Contemplation..."""
+    if helpers.get_answer():
+        print(get_hmm())
+
+
+# COMMAND ----------
+
+get_hmm()
+
+# COMMAND ----------
+
+# -*- coding: utf-8 -*-
+import helpers
+
+def get_hmm():
+    """Get a thought."""
+    return 'hmmm...'
+
+
+def hmm():
+    """Contemplation..."""
+    if helpers.get_answer():
+        print(get_hmm())
+
+
+# COMMAND ----------
+
+get_hmm()
